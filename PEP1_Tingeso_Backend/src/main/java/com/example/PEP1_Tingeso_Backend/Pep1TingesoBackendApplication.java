633e6f84803061518f7e2e64badd6fc9a49957f0
agustinsaavedra2 <agustin.saavedra@usach.cl>
2025-03-25 12:57:21 -0300

    Se conecta el proyecto a la base de datos hecha en SQL.

---
+++ PEP1_Tingeso_Backend/src/main/java/com/example/PEP1_Tingeso_Backend/Pep1TingesoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PEP1_Tingeso_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep1TingesoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pep1TingesoBackendApplication.class, args);
+	}
+
+}
