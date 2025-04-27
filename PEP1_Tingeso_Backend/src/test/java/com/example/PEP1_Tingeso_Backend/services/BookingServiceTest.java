92a1e2bafd1dac5428f7bef35185d8259be16bc3
agustinsaavedra2 <agustin.saavedra@usach.cl>
2025-04-27 01:07:52 -0400

    Cambios de nombres de paquete en Tests.

--- PEP1_Tingeso_Backend/src/test/java/com/example/PEP1_Tingeso_Backend/services/BookingServiceTest.java
+++ PEP1_Tingeso_Backend/src/test/java/com/example/PEP1_Tingeso_Backend/services/BookingServiceTest.java
@@ -1,6 +1,6 @@
-package com.example.pep1_tingeso_backend.services;
+package com.example.PEP1_Tingeso_Backend.services;
 
-import com.example.pep1_tingeso_backend.repositories.BookingRepository;
+import com.example.PEP1_Tingeso_Backend.repositories.BookingRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
--- PEP1_Tingeso_Backend/src/test/java/com/example/PEP1_Tingeso_Backend/services/VoucherServiceTest.java
+++ PEP1_Tingeso_Backend/src/test/java/com/example/PEP1_Tingeso_Backend/services/VoucherServiceTest.java
@@ -1,6 +1,6 @@
-package com.example.pep1_tingeso_backend.services;
+package com.example.PEP1_Tingeso_Backend.services;
 
-import com.example.pep1_tingeso_backend.repositories.VoucherRepository;
+import com.example.PEP1_Tingeso_Backend.repositories.VoucherRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
